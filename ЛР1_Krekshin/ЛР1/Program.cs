2fa042c890367d8a0b34ae78da4d25c88e09a32e
Dmitry <dima.krejshin@gmail.com>
2021-01-25 16:15:28 +0300

    Add ЛР1

---
+++ ЛР1_Krekshin/ЛР1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ЛР1
+{
+    class ЛР1_Krekshin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+
+            string answer = Console.ReadLine();
+
+            bool LiveBySea;
+
+            if (answer == "да")
+            {
+                LiveBySea = true;
+            }
+            else
+            {
+                LiveBySea = false;
+            }
+
+            Console.WriteLine(LiveBySea);
+        }
+    }
+}

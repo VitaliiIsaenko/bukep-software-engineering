dae5258984ab3c47a18be40f8725d8b89b5405b6
Dmitriy554 <dtrsamokhlebov@mail.ru.com>
2021-01-31 20:26:10 +0300

    add lab1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace bukep_software_engineering {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            string answer;
+            bool LiveBySea;
+            answer = Console.ReadLine();
+            if (answer == "да") {
+                LiveBySea = true;
+            }
+            else {
+                LiveBySea = false;
+            }
+            Console.WriteLine(LiveBySea);
+        }
+    }
+}

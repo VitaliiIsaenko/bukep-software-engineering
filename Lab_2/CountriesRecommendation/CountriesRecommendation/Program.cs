af9c2ab2760160fb3079dc6d42cc56900fbd697a
Dmitry <dima.krejshin@gmail.com>
2021-02-01 16:26:39 +0300

    Lab_2 complete

---
+++ Lab_2/CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CountriesRecommendation.Helpers;
+
+namespace CountriesRecommendation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            string answer = Console.ReadLine();
+            bool liveBySea;
+
+            liveBySea = answer == "да";
+
+            Console.WriteLine(liveBySea);
+
+            Country[] countries = CountriesCsvReader.ReadCountries("Krekshin.csv");
+
+            if (liveBySea) {
+                if (countries[0].HasSea) {
+                    Console.WriteLine(countries[0].ToString());
+                } else if (countries[1].HasSea) {
+                    Console.WriteLine(countries[1].ToString());
+                }
+            } else {
+                Console.WriteLine(countries[1].ToString());
+            }
+        }
+    }
+}

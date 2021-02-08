1055ae5666757358fbe51f2f3f418a6fce47b8cd
Dmitry <dima.krejshin@gmail.com>
2021-02-08 17:48:03 +0300

    checkpoint

--- Lab_2/CountriesRecommendation/CountriesRecommendation/Program.cs
+++ Lab_2/CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -15,6 +15,21 @@ namespace CountriesRecommendation
 
             Console.WriteLine(liveBySea);
 
+            //создаем двумерный массив, типа str и записываем в него результат выполнения 
+            //CsvReader(он возвращает двумерный массив из Страны в первом массиве и информации о ней внутри этого массива)
+            string[][] countriesInfo = CsvReader.Read("Isaenko.csv");
+            
+            //создаем массив countries типа Country(созданного мной класса Country) и задаем ему длинну равную размеру массива countriesInfo
+            Country[] countries = new Country[countriesInfo.Length];
+
+            for (int i = 0; i < countriesInfo.Length; i++) {
+
+                string[] countryInfo = countriesInfo[i];
+
+                countries[i] = new Country(countryInfo[0], int.Parse(countryInfo[1]), long.Parse(countryInfo[2]), countryInfo[3] == "да");
+
+            }
+
             Country[] countries = CountriesCsvReader.ReadCountries("Krekshin.csv");
 
             if (liveBySea) {

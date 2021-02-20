9d40b869d6ef1816e2ddf338f0d195cafb747a70
Vitalii Isaenko <lookyisaenko@gmail.com>
2021-02-20 11:07:15 +0100

    Complete lab-5

--- CountriesRecommendation/CountriesRecommendation/Country.cs
+++ CountriesRecommendation/CountriesRecommendation/Country.cs
@@ -1,13 +1,33 @@
-public class Country {
-    public string Name {get;}
-    public int AverageSalary {get;}
-    private long population;
-    public bool HasSea {get;}
-    
-    public Country(string countryName, int countryAverageSalary, long countryPopulation, bool countryHasSea) {
-        Name = countryName;
-        AverageSalary = countryAverageSalary;
-        population = countryPopulation;
-        HasSea = countryHasSea;
+namespace CountriesRecommendation
+{
+    public class Country
+    {
+        public string Name { get; }
+        public int AverageSalary { get; }
+        private long population;
+        public bool HasSea { get; }
+
+        public Country(string countryName, long countryPopulation, int countryAverageSalary, bool countryHasSea)
+        {
+            Name = countryName;
+            population = countryPopulation;
+            AverageSalary = countryAverageSalary;
+            HasSea = countryHasSea;
+        }
+        public CountrySize GetSize()
+        {
+            if (population <= 40_000_000)
+            {
+                return CountrySize.Small;
+            }
+            else if (40_000_000 < population && population < 100_000_000)
+            {
+                return CountrySize.Medium;
+            }
+            else
+            {
+                return CountrySize.Big;
+            }
+        }
     }
 }
--- CountriesRecommendation/CountriesRecommendation/UserPreferences.cs
+++ CountriesRecommendation/CountriesRecommendation/UserPreferences.cs
@@ -1,56 +1,77 @@
 using System;
 
-public class UserPreferences
+namespace CountriesRecommendation
 {
-    public bool Satisfied(Country country)
+    public class UserPreferences
     {
-        return LiveBySeaSatisfied(country) && MinSalarySatisfied(country);
-    }
-    private bool liveBySea;
-    private int minSalary;
-    private string size;
-private bool LiveBySeaSatisfied(Country country) {
-    //код из метода Satisfied
-    if (liveBySea)
-    {
-        if (country.HasSea)
+        private bool liveBySea;
+        private int minSalary;
+        private CountrySize size;
+        public bool Satisfied(Country country)
         {
-            return true;
+            return LiveBySeaSatisfied(country) && MinSalarySatisfied(country) && SizeSatisfied(country);
         }
-        else
+
+        private bool LiveBySeaSatisfied(Country country)
         {
+            //код из метода Satisfied
+            if (liveBySea)
+            {
+                if (country.HasSea)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool MinSalarySatisfied(Country country)
+        {
+            if (country.AverageSalary >= minSalary)
+            {
+                return true;
+            }
             return false;
         }
-    }
-    else
-    {
-        return true;
-    }
-}
 
-    private bool MinSalarySatisfied(Country country) {
-        if (country.AverageSalary >= minSalary) {
+        private bool SizeSatisfied(Country country) {
+            if (size == country.GetSize()) {
+                return true;
+            }
+            return false;
+        }
 
-            return true;
-        } 
-        return false;
-    }
 
-    public void SetLiveBySea(bool liveBySea)
-    {
-        this.liveBySea = liveBySea;
-    }
 
-public void SetMinSalary(int minSalary)
-{
-    if (minSalary > 0)
-    {
-        this.minSalary = minSalary;
-    } else {
-        // Вместо записи System.Exception можно писать просто Exception, 
-        // если в самом начале файла указать подключение пространства имен System
-        // при помощи конструкции using System;
-        throw new Exception("Минимальная зарплата должна быть больше 0");
+        public void SetLiveBySea(bool liveBySea)
+        {
+            this.liveBySea = liveBySea;
+        }
+
+        public void SetMinSalary(int minSalary)
+        {
+            if (minSalary > 0)
+            {
+                this.minSalary = minSalary;
+            }
+            else
+            {
+                // Вместо записи System.Exception можно писать просто Exception, 
+                // если в самом начале файла указать подключение пространства имен System
+                // при помощи конструкции using System;
+                throw new Exception("Минимальная зарплата должна быть больше 0");
+            }
+        }
+
+        public void SetSize(CountrySize size) {
+            this.size = size;
+        }
     }
-}
 }

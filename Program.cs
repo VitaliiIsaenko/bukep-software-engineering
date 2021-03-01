d6b413a11473a145ddc2e9001ce155e0eacb3887
Ivan Bocharov <sir.ivan-akotya@yandex.ru>
2021-03-01 12:19:29 +0300

    Выполнение лабораторной работы: 1. Установка программ. Создание первого приложения

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace bukep_software_engineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            string answer = Console.ReadLine();
+            bool LiveBySea;
+            if (answer == "Да")
+            {
+                LiveBySea = true;
+            }
+            else
+            {
+                LiveBySea = false;
+            }
+            Console.WriteLine(LiveBySea);
+        }
+    }
+}

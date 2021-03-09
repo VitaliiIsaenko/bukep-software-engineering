1e592afe31a429039827bc4e491224d3f8b2c85c
JL-INK <61227125+JL-INK@users.noreply.github.com>
2021-03-09 11:41:48 +0300

    lab1

---
+++ LabsByFrantsev/LabsByFrantsev/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LabsByFrantsev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            var answer = Console.ReadLine();
+            bool liveBySea;
+
+            if (answer == "да")
+            {
+                liveBySea = true;
+            }
+            else if (answer == "нет")
+            {
+                liveBySea = false;
+            }
+            else
+            {
+                Console.WriteLine("Введен неудовлетворяющий ответ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

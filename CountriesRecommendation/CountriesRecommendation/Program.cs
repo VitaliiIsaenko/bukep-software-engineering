3fafe5abe3d9ceb9793568a0c21cfc8903b75c51
Ivan Bocharov <sir.ivan-akotya@yandex.ru>
2021-03-09 11:20:18 +0300

    Выполнение второй лабораторной работы

---
+++ CountriesRecommendation/CountriesRecommendation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CountriesRecommendation.Helpers;
+namespace CountriesRecommendation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Хотите ли вы жить у моря?");
+            string answer = Console.ReadLine();
+            bool liveBySea;
+
+            liveBySea = answer == "да";
+
+
+
+            Country[] countries = CountriesCsvReader.ReadCountries("Isaenko.csv");
+            if (answer == "да")
+            {
+                if (countries[0].HasSea == true)
+                {
+                    Console.WriteLine(countries[0].ToString());
+                }
+                else
+                {
+                Console.WriteLine(countries[1].ToString());
+                }
+            }
+            else
+            {
+                if (countries[0].HasSea == false)
+                {
+                    Console.WriteLine(countries[0].ToString());
+                }
+                else
+                {
+                Console.WriteLine(countries[1].ToString());
+                }
+            }
+        }
+    }
+}
--- Examples/Lecture06-demo/Portfolio.cs
+++
@@ -1,9 +0,0 @@
-public class Portfolio {
-    public string name;
-    //Кол-во страниц
-    public int pagesAmount;
-
-    public string GetFullName() {
-        return name + " " + pagesAmount;
-    }
-}

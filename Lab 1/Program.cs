375a46ceb7fef605d8e0665b307657919f354779
Liza <lizaaa44556677@gmail.com>
2021-03-15 17:48:05 +0300

    1 лабр

---
+++ Lab 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bukep_software_engineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("хотите ли вы жить у моря?");
+            String answer = Console.ReadLine();
+            bool liveBySea;
+            
+            if (answer == "да")
+            {
+                liveBySea = true;
+            }
+            else
+            {
+                liveBySea = false;
+            }
+            Console.WriteLine(liveBySea);
+        }
+    }
+}

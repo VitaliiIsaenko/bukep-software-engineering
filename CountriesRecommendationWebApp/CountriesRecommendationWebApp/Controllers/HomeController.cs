c9c343d29914c9e22b8a70f7bb0ca24fde1bcb76
Liza <lizaaa44556677@gmail.com>
2021-04-03 14:26:09 +0300

    Лабр 8

--- CountriesRecommendationWebApp/CountriesRecommendationWebApp/Controllers/HomeController.cs
+++ CountriesRecommendationWebApp/CountriesRecommendationWebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CountriesRecommendation.Helpers;
+using CountriesRecommendationWebApp.Enums;
+using CountriesRecommendationWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CountriesRecommendationWebApp.Controllers
 {
@@ -11,9 +14,36 @@ namespace CountriesRecommendationWebApp.Controllers
         {
             return View();
         }
-        public IActionResult CountryInfo()
+
+        [HttpPost]
+        public IActionResult CountryInfo(bool liveBySea, int minSalary, CountrySize size)
         {
-            return View();
+            // На основе полученных параметров формируем объект типа UserPreferences
+            UserPreferences userPreferences = new UserPreferences(liveBySea, minSalary, size);
+
+            // Считываем данные о странах в виде массивов строк
+            string[][] countriesStrings = CsvReader.Read(new string[] {"Homchenko.csv"});
+            Country[] countries = new Country[countriesStrings.Length];
+
+            // Каждую строку массива преобразуем в объект и заполняем объектами массив countries
+            for (int i = 0; i < countriesStrings.Length; i++) {
+                string[] countryString = countriesStrings[i];
+                countries[i] = new Country(countryString[0], long.Parse(countryString[1]),
+                int.Parse(countryString[2]), countryString[3] == "да");
+            }
+
+            // Определяем переменную, в которой будем хранить выбранную страну
+            Country foundCountry = null;
+
+            // В цикле ищем страну, удовлетворяющую предпочтениям пользователя
+            foreach (Country country in countries) {
+                if (userPreferences.Satisfied(country)) {
+                    foundCountry = country;
+                }
+            }
+
+            // Возвращаем в ответ объект представления, куда передаем переменную с выбранной страной 
+            return View(foundCountry);
         }
     }
 }
